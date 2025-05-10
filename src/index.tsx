b1e3add8ffd615b5be5412bddfcba0c087e88ecd
Felix Navas <felix.navas@designli.co>
2025-05-10 00:35:35 -0600

    style: set-up initial style for light and dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { render } from 'preact';
 
 import preactLogo from './assets/preact.svg';
-import './style.css';
+import './styles/global.css';
 
 export function App() {
 	return (
 		<div>
-			<a href="https://preactjs.com" target="_blank">
+			<a href="https://preactjs.com" target="_blank" rel="noreferrer">
 				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
 			</a>
 			<h1>Get Started building Vite-powered Preact Apps </h1>
@@ -33,7 +33,7 @@ export function App() {
 
 function Resource(props) {
 	return (
-		<a href={props.href} target="_blank" class="resource">
+		<a href={props.href} target="_blank" class="resource" rel="noreferrer">
 			<h2>{props.title}</h2>
 			<p>{props.description}</p>
 		</a>
